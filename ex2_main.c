d6335fe054ca6e279aaad098b835467ccd1bf673
jdsilveira-code <jd.silverg@gmail.com>
2025-11-29 12:58:29 -0300

    Update ex2_main.c

--- ex2_main.c
+++ ex2_main.c
@@ -1,1 +1,24 @@
 //Carlos Barroca Faria e José Davi Silveira Gomes 29/11
+#include <stdio.h>
+#include "ex2.h"
+
+int main(){
+    Lista *minhaLista = lst_cria();
+    
+    printf("=== Teste Exercicio 2: Insercao no Final ===\n");
+
+    minhaLista = lst_insereFinal(10, minhaLista);
+    lst_imprime(minhaLista); 
+
+    minhaLista = lst_insereFinal(20, minhaLista);
+    minhaLista = lst_insereFinal(30, minhaLista);
+    lst_imprime(minhaLista);
+
+    minhaLista = lst_insere(5, minhaLista); 
+    lst_imprime(minhaLista);
+    
+    printf("Tamanho final: %d\n", lst_lenght(minhaLista));
+
+    lst_libera(minhaLista);
+    return 0;
+}
