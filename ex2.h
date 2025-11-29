f2797b3d26ea1a11ae3e9d3f6491185758f4f479
jdsilveira-code <jd.silverg@gmail.com>
2025-11-29 12:58:31 -0300

    Update ex2.h

--- ex2.h
+++ ex2.h
@@ -1,1 +1,17 @@
 //Carlos Barroca Faria e José Davi Silveira Gomes 29/11
+#ifndef EX2_H
+#define EX2_H
+
+typedef struct celula Lista;
+
+Lista* lst_cria(void);
+void lst_libera(Lista *lista);
+Lista* lst_insere(int conteudo, Lista *lista);
+void lst_imprime(Lista *lista);
+Lista* lst_retira(int conteudo, Lista *lista);
+
+int lst_lenght(Lista *lista);
+
+Lista* lst_insereFinal(int conteudo, Lista *lista);
+
+#endif
