d4957348bf8624067e30b5b8ec6ab3bdb8bdb5ea
jdsilveira-code <jd.silverg@gmail.com>
2025-11-29 12:58:33 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,80 @@
 //Carlos Barroca Faria e José Davi Silveira Gomes 29/11
+#include <stdio.h>
+#include <stdlib.h>
+#include "ex2.h"
+
+struct celula {
+    int conteudo;
+    struct celula *prox;
+};
+
+Lista* lst_cria(void) { return NULL; }
+
+Lista* lst_insere(int conteudo, Lista *lista) {
+    Lista *nova = (Lista*) malloc(sizeof(Lista));
+    nova->conteudo = conteudo;
+    nova->prox = lista;
+    return nova;
+}
+
+void lst_imprime(Lista *lista) {
+    Lista *p;
+    printf("Lista: ");
+    for (p = lista; p != NULL; p = p->prox) {
+        printf("%d -> ", p->conteudo);
+    }
+    printf("NULL\n");
+}
+
+void lst_libera(Lista *lista) {
+    Lista *p = lista;
+    while (p != NULL) {
+        Lista *t = p->prox;
+        free(p);
+        p = t;
+    }
+}
+
+Lista* lst_retira(int conteudo, Lista *lista) {
+    Lista *ant = NULL;
+    Lista *p = lista;
+    while (p != NULL && p->conteudo != conteudo) {
+        ant = p;
+        p = p->prox;
+    }
+    if (p == NULL) return lista;
+    if (ant == NULL) lista = p->prox;
+    else ant->prox = p->prox;
+    free(p);
+    return lista;
+}
+
+int lst_lenght(Lista *lista) {
+    int contador = 0;
+    Lista *p;
+    for (p = lista; p != NULL; p = p->prox) contador++;
+    return contador;
+}
+
+Lista* lst_insereFinal(int conteudo, Lista *lista) {
+    Lista *p = lista;
+    
+
+    Lista *nova = (Lista*) malloc(sizeof(Lista));
+    if (nova == NULL) exit(1);
+    
+    nova->conteudo = conteudo;
+    nova->prox = NULL; 
+
+    if (lista == NULL) {
+        return nova; 
+    }
+
+    while (p->prox != NULL) {
+        p = p->prox;
+    }
+
+    p->prox = nova;
+
+    return lista; 
+}
