df1ba3fad136189bc6c611f256e83cc76f254e85
jdsilveira-code <jd.silverg@gmail.com>
2025-11-29 12:58:26 -0300

    Create ex3.h

---
+++ ex3.h
@@ -0,0 +1,17 @@
+#ifndef EX3_H
+#define EX3_H
+
+typedef struct celula Lista;
+
+Lista* lst_cria(void);
+void lst_libera(Lista *lista);
+Lista* lst_insere(int conteudo, Lista *lista);
+void lst_imprime(Lista *lista);
+Lista* lst_retira(int conteudo, Lista *lista);
+
+int lst_lenght(Lista *lista);
+Lista* lst_insereFinal(int conteudo, Lista *lista);
+
+Lista* lst_busca(int conteudo, Lista *lista);
+
+#endif
